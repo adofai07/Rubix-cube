a1fce0daed364e119836023898557756b03fd625
adofai07 <chldmsgh695@gmail.com>
2023-06-20 16:38:09 +0900

    Just trolling

--- score_ai.py
+++ score_ai.py
@@ -2,20 +2,28 @@ import tensorflow as tf
 from type import Cube, scrambled_cube, initial_cube
 import tqdm
 import random
+import numpy as np
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(16, activation='relu', input_shape=(54, )),
-    tf.keras.layers.Dense(4, activation='relu'),
-    tf.keras.layers.Dense(1, activation='softmax')
+    tf.keras.layers.Dense(256, activation='sigmoid', input_shape=(54, )),
+    tf.keras.layers.Dense(64, activation='sigmoid'),
+    tf.keras.layers.Dense(16, activation='sigmoid'),
+    tf.keras.layers.Dense(4, activation='sigmoid'),
+    tf.keras.layers.Dense(1, activation='linear')
 ])
 
 model.compile(optimizer="Adam", loss="mse", metrics=["accuracy"])
 
-I_O = [(initial_cube(), 1000)]
+I_O = list()
 
-for i in tqdm.trange(1, 16, ascii=True, position=0):
+for i in tqdm.trange(16, ascii=True, position=0):
     for j in tqdm.trange(100000, ascii=True, position=1, leave=False):
-        I_O.append((scrambled_cube(i), -i))
+        I_O.append((scrambled_cube(i).arr[1:], -i if i != 0 else 5))
 
 random.shuffle(I_O)
+
+inputs = np.asarray([i[0] for i in I_O])
+outputs = np.asarray([i[1] for i in I_O])
+
+history = model.fit(inputs, outputs, epochs=20, batch_size=20)
 
