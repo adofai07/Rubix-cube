b16ab789a3f269e50da3f8d7755d206e1e5e74a5
adofai07 <chldmsgh695@gmail.com>
2023-06-22 22:35:09 +0900

    Bugfix

--- AIModel.py
+++ AIModel.py
@@ -6,7 +6,7 @@ import numpy as np
 import pickle
 import os
 import sys
-from path import checkpoint_path, save_path
+from path import checkpoint_path, save_path, load_path
 
 print(tf.__version__)
 print(tf.config.list_physical_devices('GPU'))
@@ -15,29 +15,37 @@ def save(data, thread=0):
     with open(FR"C:\SSHS\codes\Projects\Rubix-cube\G{thread}.adofai", "wb+") as f:
         pickle.dump(data, f)
 
+def load(thread=0):
+    while True:
+        try:
+            with open(FR"C:\SSHS\codes\Projects\Rubix-cube\G{thread}.adofai", "rb") as f:
+                return pickle.load(f)
+            
+        except:
+            ...
 
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                 save_weights_only=False,
-                                                 verbose=1)
+# Error if set to True
+FROM_SAVE = False
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(3125, activation='sigmoid', input_shape=(54, )),
-    tf.keras.layers.Dense(625, activation='sigmoid'),
-    tf.keras.layers.Dense(125, activation='sigmoid'),
-    tf.keras.layers.Dense(25, activation='sigmoid'),
-    tf.keras.layers.Dense(5, activation='sigmoid'),
-    tf.keras.layers.Dense(1, activation='linear')
-])
+if FROM_SAVE:
+    model = tf.keras.models.load_model(load_path)
 
-# Comment this line if this is your first time training
-model = tf.keras.models.load_model(save_path)
+else:
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(3125, activation='sigmoid', input_shape=(54, )),
+        tf.keras.layers.Dense(625, activation='sigmoid'),
+        tf.keras.layers.Dense(125, activation='sigmoid'),
+        tf.keras.layers.Dense(25, activation='sigmoid'),
+        tf.keras.layers.Dense(5, activation='sigmoid'),
+        tf.keras.layers.Dense(1, activation='linear')
+    ])
 
-model.compile(optimizer="Adam", loss="mse", metrics=[])
+    model.compile(optimizer="Adam", loss="mse", metrics=[])
 
 I_O = list()
 
 for i in tqdm.trange(16, ascii=True, position=0):
-    for j in tqdm.trange(10000, ascii=True, position=1, leave=False):
+    for j in tqdm.trange(2000, ascii=True, position=1, leave=False):
         I_O.append((scrambled_cube(i).arr[1:], -i))
 
 random.shuffle(I_O)
@@ -50,10 +58,12 @@ print(F"\nData: {sys.getsizeof(inputs) :,} bytes (input), {sys.getsizeof(outputs
 history = model.fit(
     inputs,
     outputs,
-    epochs=100,
-    batch_size=100,
-    # callbacks=[cp_callback]
+    epochs=1,
+    batch_size=20
 )
 
-os.remove(save_path)
-model.save(save_path)
+if os.path.exists(save_path):
+    os.remove(save_path)
+    print("Deleted!")
+
+tf.keras.models.save_model(model, save_path, overwrite=True, save_format="hdf5")
--- path.py
+++ path.py
@@ -1,4 +1,5 @@
 import os
 
 checkpoint_path = os.getenv("APPDATA") + R"\Rubix-cube.ckpt"
-save_path = R"C:\SSHS\codes\Projects\Rubix-cube\Model.h5"
+save_path = R"C:\SSHS\codes\Projects\Rubix-cube\ModelA.h5"
+load_path = R"C:\SSHS\codes\Projects\Rubix-cube2\ModelA.h5"
